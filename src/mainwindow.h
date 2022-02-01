ef23316d417bd25ca48157e26b54f47f86290c65
Will Sciaroni <wsciaroni@gmail.com>
2022-02-01 17:47:19 -0600

    Incorporate Generated Config from QtCreator

---
+++ src/mainwindow.h
@@ -0,0 +1,23 @@
+#ifndef CLUE_MAINWINDOW_H
+#define CLUE_MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Clue {
+
+namespace Ui { class MainWindow; }
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+} // namespace Clue
+#endif // CLUE_MAINWINDOW_H
