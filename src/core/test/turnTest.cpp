30e9fcbd8c79efff180fa4024cdb93195162e98d
William Sciaroni <wsciaroni@protonmail.com>
2023-02-19 03:18:08 +0000

    Add More Turn tests for total code coverage

--- src/core/test/turnTest.cpp
+++ src/core/test/turnTest.cpp
@@ -26,6 +26,7 @@ protected:
     std::shared_ptr<Clue::Player> p0 = std::make_shared<Clue::Player>();
     std::shared_ptr<Clue::Player> p1 = std::make_shared<Clue::Player>();
     std::shared_ptr<Clue::Player> p2 = std::make_shared<Clue::Player>();
+    std::shared_ptr<Clue::Player> p3 = std::make_shared<Clue::Player>();
 
     std::set<std::shared_ptr<Clue::Player>> playersWithoutCardsSet;
 
@@ -33,6 +34,7 @@ protected:
         p0->setPlayerId(Clue::PlayerId::PLAYER_0);
         p1->setPlayerId(Clue::PlayerId::PLAYER_1);
         p2->setPlayerId(Clue::PlayerId::PLAYER_2);
+        p3->setPlayerId(Clue::PlayerId::PLAYER_3);
     }
 
     void TearDown() override {}
@@ -103,4 +105,149 @@ TEST_F(TurnTestF, SecondPlayerAnswers) {
     EXPECT_TRUE(p1->doesntHaveCard(Clue::Card::MISS_SCARLET));
     EXPECT_TRUE(p1->doesntHaveCard(Clue::Card::CANDLESTICK));
     EXPECT_TRUE(p1->doesntHaveCard(Clue::Card::KITCHEN));
+
+    auto s = t.toString();
+}
+
+TEST_F(TurnTestF, OtherPlayerAnswers) {
+    playersWithoutCardsSet.insert(p2);
+    Clue::Turn t(
+        p1,
+        true,
+        Clue::Suspect::MISS_SCARLET,
+        Clue::Weapon::CANDLESTICK,
+        Clue::Room::KITCHEN,
+        p3,
+        Clue::Card::INVALID,
+        playersWithoutCardsSet
+    );
+    EXPECT_FALSE(t.getIsMyTurn());
+    EXPECT_FALSE(t.getIAnswered());
+    EXPECT_EQ(t.getPlayersTurn(), p1);
+    EXPECT_EQ(t.getPlayersWithoutCards(), playersWithoutCardsSet);
+
+    EXPECT_EQ(t.getAccusationSuspect(), Clue::Suspect::MISS_SCARLET);
+    EXPECT_EQ(t.getAccusationWeapon(),Clue::Weapon::CANDLESTICK);
+    EXPECT_EQ(t.getAccusationRoom(),Clue::Room::KITCHEN);
+    EXPECT_EQ(t.getPlayerAnswered(), p3);
+    EXPECT_EQ(t.getCardShown(), Clue::Card::INVALID);
+
+// We now know, p1 does not have any of these 3 cards:
+    EXPECT_TRUE(p2->doesntHaveCard(Clue::Card::MISS_SCARLET));
+    EXPECT_TRUE(p2->doesntHaveCard(Clue::Card::CANDLESTICK));
+    EXPECT_TRUE(p2->doesntHaveCard(Clue::Card::KITCHEN));
+}
+
+TEST_F(TurnTestF, OtherPlayerNoOneAnswers) {
+    playersWithoutCardsSet.insert(p0);
+    playersWithoutCardsSet.insert(p2);
+    playersWithoutCardsSet.insert(p3);
+    Clue::Turn t(
+        p1,
+        true,
+        Clue::Suspect::MISS_SCARLET,
+        Clue::Weapon::CANDLESTICK,
+        Clue::Room::KITCHEN,
+        p1,
+        Clue::Card::INVALID,
+        playersWithoutCardsSet
+    );
+    EXPECT_FALSE(t.getIsMyTurn());
+    EXPECT_FALSE(t.getIAnswered());
+    EXPECT_EQ(t.getPlayersTurn(), p1);
+    EXPECT_EQ(t.getPlayersWithoutCards(), playersWithoutCardsSet);
+
+    EXPECT_EQ(t.getAccusationSuspect(), Clue::Suspect::MISS_SCARLET);
+    EXPECT_EQ(t.getAccusationWeapon(),Clue::Weapon::CANDLESTICK);
+    EXPECT_EQ(t.getAccusationRoom(),Clue::Room::KITCHEN);
+    EXPECT_EQ(t.getPlayerAnswered(), p1);
+    EXPECT_EQ(t.getCardShown(), Clue::Card::INVALID);
+
+// We now know, p1 does not have any of these 3 cards:
+    EXPECT_TRUE(p2->doesntHaveCard(Clue::Card::MISS_SCARLET));
+    EXPECT_TRUE(p2->doesntHaveCard(Clue::Card::CANDLESTICK));
+    EXPECT_TRUE(p2->doesntHaveCard(Clue::Card::KITCHEN));
+
+    EXPECT_TRUE(p3->doesntHaveCard(Clue::Card::MISS_SCARLET));
+    EXPECT_TRUE(p3->doesntHaveCard(Clue::Card::CANDLESTICK));
+    EXPECT_TRUE(p3->doesntHaveCard(Clue::Card::KITCHEN));
+
+    EXPECT_TRUE(p0->doesntHaveCard(Clue::Card::MISS_SCARLET));
+    EXPECT_TRUE(p0->doesntHaveCard(Clue::Card::CANDLESTICK));
+    EXPECT_TRUE(p0->doesntHaveCard(Clue::Card::KITCHEN));
+
+    auto s = t.toString();
+}
+
+TEST_F(TurnTestF, OtherPlayerNoAccusation) {
+    Clue::Turn t(
+        p1,
+        false,
+        Clue::Suspect::NONE,
+        Clue::Weapon::NONE,
+        Clue::Room::NONE,
+        p1,
+        Clue::Card::NONE,
+        playersWithoutCardsSet
+    );
+    EXPECT_FALSE(t.getIsMyTurn());
+    EXPECT_FALSE(t.getIAnswered());
+    EXPECT_EQ(t.getPlayersTurn(), p1);
+    EXPECT_EQ(t.getPlayersWithoutCards(), playersWithoutCardsSet);
+
+    EXPECT_EQ(t.getAccusationSuspect(), Clue::Suspect::NONE);
+    EXPECT_EQ(t.getAccusationWeapon(),Clue::Weapon::NONE);
+    EXPECT_EQ(t.getAccusationRoom(),Clue::Room::NONE);
+    EXPECT_EQ(t.getPlayerAnswered(), p1);
+    EXPECT_EQ(t.getCardShown(), Clue::Card::NONE);
+}
+
+TEST_F(TurnTestF, MeNoAccusation) {
+    Clue::Turn t(
+        p0,
+        false,
+        Clue::Suspect::NONE,
+        Clue::Weapon::NONE,
+        Clue::Room::NONE,
+        p0,
+        Clue::Card::NONE,
+        playersWithoutCardsSet
+    );
+    EXPECT_TRUE(t.getIsMyTurn());
+    EXPECT_FALSE(t.getIAnswered());
+    EXPECT_EQ(t.getPlayersTurn(), p0);
+    EXPECT_EQ(t.getPlayersWithoutCards(), playersWithoutCardsSet);
+
+    EXPECT_EQ(t.getAccusationSuspect(), Clue::Suspect::NONE);
+    EXPECT_EQ(t.getAccusationWeapon(),Clue::Weapon::NONE);
+    EXPECT_EQ(t.getAccusationRoom(),Clue::Room::NONE);
+    EXPECT_EQ(t.getPlayerAnswered(), p0);
+    EXPECT_EQ(t.getCardShown(), Clue::Card::NONE);
+}
+
+TEST_F(TurnTestF, MeShowP3) {
+    p1->addCardToHand(Clue::Card::MISS_SCARLET);
+    playersWithoutCardsSet.insert(p3);
+    Clue::Turn t(
+        p2,
+        true,
+        Clue::Suspect::MISS_SCARLET,
+        Clue::Weapon::CANDLESTICK,
+        Clue::Room::KITCHEN,
+        p0,
+        Clue::Card::MISS_SCARLET,
+        playersWithoutCardsSet
+    );
+    EXPECT_FALSE(t.getIsMyTurn());
+    EXPECT_TRUE(t.getIAnswered());
+    EXPECT_EQ(t.getPlayersTurn(), p2);
+    EXPECT_EQ(t.getPlayersWithoutCards(), playersWithoutCardsSet);
+
+    EXPECT_EQ(t.getAccusationSuspect(), Clue::Suspect::MISS_SCARLET);
+    EXPECT_EQ(t.getAccusationWeapon(),Clue::Weapon::CANDLESTICK);
+    EXPECT_EQ(t.getAccusationRoom(),Clue::Room::KITCHEN);
+    EXPECT_EQ(t.getPlayerAnswered(), p0);
+    EXPECT_EQ(t.getCardShown(), Clue::Card::MISS_SCARLET);
+
+    auto s = t.toString();
 }
