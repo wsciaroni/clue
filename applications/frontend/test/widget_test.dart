e2d35783cc1ce93707b680a3bd8a42d3d7f52356
William Sciaroni <wsciaroni@gmail.com>
2026-01-03 09:25:32 -0600

    Revert "Resolve all Flutter analysis issues"
    This reverts commit ffc56884c1f0ba738a492fb1f508b6c5fa159a73.

--- applications/frontend/test/widget_test.dart
+++ applications/frontend/test/widget_test.dart
@@ -7,16 +7,6 @@
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:frontend/main.dart';
-
 void main() {
-  testWidgets('App starts with Setup Screen', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const ClueApp());
-
-    // Verify that we start on the Setup Screen
-    expect(find.text('Clue Setup'), findsOneWidget);
-    expect(find.text('Players'), findsOneWidget);
-    expect(find.text('My Hand'), findsOneWidget);
-  });
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {});
 }
