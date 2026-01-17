b67cc9fb24225ddebe4168b8b507eb66c2179647
William Sciaroni <wsciaroni@protonmail.com>
2026-01-17 18:44:50 +0000

    Make shallow checkout

--- cmake/EnableGTest.cmake
+++ cmake/EnableGTest.cmake
@@ -13,6 +13,8 @@ macro(enable_gtest)
             googletest
             GIT_REPOSITORY https://github.com/google/googletest.git
             GIT_TAG v1.15.2
+            GIT_PROGRESS TRUE
+            GIT_SHALLOW TRUE
         )
         # For Windows: Prevent overriding the parent project's compiler/linker settings
         if(MSVC)
