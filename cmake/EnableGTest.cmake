9b661bf31de78c44a7f5d652195be46646584265
William Sciaroni <wsciaroni@protonmail.com>
2026-01-17 18:46:11 +0000

    Bump Googletest to latest

--- cmake/EnableGTest.cmake
+++ cmake/EnableGTest.cmake
@@ -12,7 +12,7 @@ macro(enable_gtest)
         FetchContent_Declare(
             googletest
             GIT_REPOSITORY https://github.com/google/googletest.git
-            GIT_TAG v1.15.2
+            GIT_TAG v1.17.0
             GIT_PROGRESS TRUE
             GIT_SHALLOW TRUE
         )
