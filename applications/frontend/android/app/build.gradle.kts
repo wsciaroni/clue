37693299740781e33ae6c76ff84fc9035dfd8a68
William Sciaroni <wsciaroni@protonmail.com>
2026-01-17 19:19:30 +0000

    Add obfuscation

--- applications/frontend/android/app/build.gradle.kts
+++ applications/frontend/android/app/build.gradle.kts
@@ -32,6 +32,11 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
